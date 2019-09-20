f14340941da7bf333e3bfb4b4b963352d630a92d
vagrant <vagrant@localhost.localdomain>
2019-09-20 15:41:28 +0000

    ユーザー情報更新バグ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -83,6 +83,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+  end
+
   def edit_basic_info
     @user = User.find(params[:id])
   end
